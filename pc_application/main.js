b38e349bd028094c96adaab31734fca4ec1a7d91
TheGreatRambler <aehart684351@gmail.com>
2020-01-09 16:02:16 -0500

    Switch to a NodeJS implementation

---
+++ pc_application/main.js
@@ -0,0 +1,42 @@
+const WebSocket = require('ws');
+const Gamepad = require("./gamepadApi/gamepad");
+const Ip = require("ip");
+
+const PORT = 6954;
+// Milliseconds betwen update
+const UPDATE_RATE = 16;
+
+// Start server
+const wss;
+
+function setupServer() {
+	wss = new WebSocket.Server({
+		port: PORT
+	});
+
+	// Listen for gamepad connections
+	wss.on("connection", function(ws) {
+		ws.on("message", function(message) {
+			console.log('received: %s', message);
+		});
+
+		ws.send('something');
+	});
+}
+
+function listGamepads() {
+	for (let i = 0; i < Gamepad.numDevices(); i++) {
+		// Print device here
+		console.log(i, gamepad.deviceAtIndex());
+	}
+}
+
+function init() {
+	// Setup WS server
+	setupServer();
+	Gamepad.init();
+	// Start main loop
+	setInterval(function() {
+
+	})
+}
